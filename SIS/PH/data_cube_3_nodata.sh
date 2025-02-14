ba70a498bf144768b7a82c0ba62ac07d98415868
eloi=ribeiro <mail@eloiribeiro.eu>
2025-02-14 10:47:36 +0100

    soil properties must have at least 3 char GISMGR

--- SIS/PH/data_cube_3_nodata.sh
+++ SIS/PH/data_cube_3_nodata.sh
@@ -14,12 +14,12 @@ echo
 echo "Assigning NoData to $NODATA ..."
 
 # NoData NULL
-gdal_calc.py --quiet -A PH-GSAS-EC-2020-0-30.tif --outfile=temp.tif --calc="A*(A!=0.23120065033435822) + ($NODATA)*(A==0.23120065033435822)" --NoDataValue=$NODATA && mv temp.tif PH-GSAS-EC-2020-0-30.tif
-gdal_calc.py --quiet -A PH-GSAS-EC-2020-30-100.tif --outfile=temp.tif --calc="A*(A!=0.1270795613527298) + ($NODATA)*(A==0.1270795613527298)" --NoDataValue=$NODATA && mv temp.tif PH-GSAS-EC-2020-30-100.tif
+gdal_calc.py --quiet -A PH-GSAS-ECX-2020-0-30.tif --outfile=temp.tif --calc="A*(A!=0.23120065033435822) + ($NODATA)*(A==0.23120065033435822)" --NoDataValue=$NODATA && mv temp.tif PH-GSAS-ECX-2020-0-30.tif
+gdal_calc.py --quiet -A PH-GSAS-ECX-2020-30-100.tif --outfile=temp.tif --calc="A*(A!=0.1270795613527298) + ($NODATA)*(A==0.1270795613527298)" --NoDataValue=$NODATA && mv temp.tif PH-GSAS-ECX-2020-30-100.tif
 gdal_calc.py --quiet -A PH-GSAS-ESP-2020-0-30.tif --outfile=temp.tif --calc="A*(A!=3.2343051433563232) + ($NODATA)*(A==3.2343051433563232)" --NoDataValue=$NODATA && mv temp.tif PH-GSAS-ESP-2020-0-30.tif
 gdal_calc.py --quiet -A PH-GSAS-ESP-2020-30-100.tif --outfile=temp.tif --calc="A*(A!=6.29727840423584) + ($NODATA)*(A==6.29727840423584)" --NoDataValue=$NODATA && mv temp.tif PH-GSAS-ESP-2020-30-100.tif
-gdal_calc.py --quiet -A PH-GSAS-PH-2020-0-30.tif --outfile=temp.tif --calc="A*(A!=6.248271942138672) + ($NODATA)*(A==6.248271942138672)" --NoDataValue=$NODATA && mv temp.tif PH-GSAS-PH-2020-0-30.tif
-gdal_calc.py --quiet -A PH-GSAS-PH-2020-30-100.tif --outfile=temp.tif --calc="A*(A!=6.0150837898254395) + ($NODATA)*(A==6.0150837898254395)" --NoDataValue=$NODATA && mv temp.tif PH-GSAS-PH-2020-30-100.tif
+gdal_calc.py --quiet -A PH-GSAS-PHX-2020-0-30.tif --outfile=temp.tif --calc="A*(A!=6.248271942138672) + ($NODATA)*(A==6.248271942138672)" --NoDataValue=$NODATA && mv temp.tif PH-GSAS-PHX-2020-0-30.tif
+gdal_calc.py --quiet -A PH-GSAS-PHX-2020-30-100.tif --outfile=temp.tif --calc="A*(A!=6.0150837898254395) + ($NODATA)*(A==6.0150837898254395)" --NoDataValue=$NODATA && mv temp.tif PH-GSAS-PHX-2020-30-100.tif
 # gdal_calc.py --quiet -A PH-GSAS-SALT-2020-0-30.tif --outfile=temp.tif --calc="A*(A!=1) + ($NODATA)*(A==1)" --NoDataValue=$NODATA && mv temp.tif PH-GSAS-SALT-2020-0-30.tif
 # gdal_calc.py --quiet -A PH-GSAS-SALT-2020-30-100.tif --outfile=temp.tif --calc="A*(A!=2) + ($NODATA)*(A==2)" --NoDataValue=$NODATA && mv temp.tif PH-GSAS-SALT-2020-30-100.tif
 
@@ -37,20 +37,20 @@ gdal_calc.py --quiet -A PH-GSOC-SOCBAU-2021-0-30.tif --outfile=temp.tif --calc="
 gdal_calc.py --quiet -A PH-GSOC-SOCSSM1-2021-0-30.tif --outfile=temp.tif --calc="A*(A!=-99999) + ($NODATA)*(A==-99999)" --NoDataValue=$NODATA && mv temp.tif PH-GSOC-SOCSSM1-2021-0-30.tif
 gdal_calc.py --quiet -A PH-GSOC-SOCSSM2-2021-0-30.tif --outfile=temp.tif --calc="A*(A!=-99999) + ($NODATA)*(A==-99999)" --NoDataValue=$NODATA && mv temp.tif PH-GSOC-SOCSSM2-2021-0-30.tif
 gdal_calc.py --quiet -A PH-GSOC-SOCSSM3-2021-0-30.tif --outfile=temp.tif --calc="A*(A!=-99999) + ($NODATA)*(A==-99999)" --NoDataValue=$NODATA && mv temp.tif PH-GSOC-SOCSSM3-2021-0-30.tif
-gdal_calc.py --quiet -A PH-GSNM-BD-2023-0-30.tif --outfile=temp.tif --calc="A*(A!=-9999) + ($NODATA)*(A==-9999)" --NoDataValue=$NODATA && mv temp.tif PH-GSNM-BD-2023-0-30.tif 
-gdal_calc.py --quiet -A PH-GSNM-BD-2023-30-60.tif --outfile=temp.tif --calc="A*(A!=-9999) + ($NODATA)*(A==-9999)" --NoDataValue=$NODATA && mv temp.tif PH-GSNM-BD-2023-30-60.tif 
+gdal_calc.py --quiet -A PH-GSNM-BKD-2023-0-30.tif --outfile=temp.tif --calc="A*(A!=-9999) + ($NODATA)*(A==-9999)" --NoDataValue=$NODATA && mv temp.tif PH-GSNM-BKD-2023-0-30.tif 
+gdal_calc.py --quiet -A PH-GSNM-BKD-2023-30-60.tif --outfile=temp.tif --calc="A*(A!=-9999) + ($NODATA)*(A==-9999)" --NoDataValue=$NODATA && mv temp.tif PH-GSNM-BKD-2023-30-60.tif 
 gdal_calc.py --quiet -A PH-GSNM-CEC-2023-0-30.tif --outfile=temp.tif --calc="A*(A!=-9999) + ($NODATA)*(A==-9999)" --NoDataValue=$NODATA && mv temp.tif PH-GSNM-CEC-2023-0-30.tif 
 gdal_calc.py --quiet -A PH-GSNM-CEC-2023-30-60.tif --outfile=temp.tif --calc="A*(A!=-9999) + ($NODATA)*(A==-9999)" --NoDataValue=$NODATA && mv temp.tif PH-GSNM-CEC-2023-30-60.tif 
 gdal_calc.py --quiet -A PH-GSNM-CLAY-2023-0-30.tif --outfile=temp.tif --calc="A*(A!=-9999) + ($NODATA)*(A==-9999)" --NoDataValue=$NODATA && mv temp.tif PH-GSNM-CLAY-2023-0-30.tif 
 gdal_calc.py --quiet -A PH-GSNM-CLAY-2023-30-60.tif --outfile=temp.tif --calc="A*(A!=-9999) + ($NODATA)*(A==-9999)" --NoDataValue=$NODATA && mv temp.tif PH-GSNM-CLAY-2023-30-60.tif 
-gdal_calc.py --quiet -A PH-GSNM-K-2023-0-30.tif --outfile=temp.tif --calc="A*(A!=-9999) + ($NODATA)*(A==-9999)" --NoDataValue=$NODATA && mv temp.tif PH-GSNM-K-2023-0-30.tif 
-gdal_calc.py --quiet -A PH-GSNM-K-2023-30-60.tif --outfile=temp.tif --calc="A*(A!=-9999) + ($NODATA)*(A==-9999)" --NoDataValue=$NODATA && mv temp.tif PH-GSNM-K-2023-30-60.tif 
-gdal_calc.py --quiet -A PH-GSNM-OC-2023-0-30.tif --outfile=temp.tif --calc="A*(A!=-9999) + ($NODATA)*(A==-9999)" --NoDataValue=$NODATA && mv temp.tif PH-GSNM-OC-2023-0-30.tif 
-gdal_calc.py --quiet -A PH-GSNM-OC-2023-30-60.tif --outfile=temp.tif --calc="A*(A!=-9999) + ($NODATA)*(A==-9999)" --NoDataValue=$NODATA && mv temp.tif PH-GSNM-OC-2023-30-60.tif 
-gdal_calc.py --quiet -A PH-GSNM-P-2023-0-30.tif --outfile=temp.tif --calc="A*(A!=-9999) + ($NODATA)*(A==-9999)" --NoDataValue=$NODATA && mv temp.tif PH-GSNM-P-2023-0-30.tif 
-gdal_calc.py --quiet -A PH-GSNM-P-2023-30-60.tif --outfile=temp.tif --calc="A*(A!=-9999) + ($NODATA)*(A==-9999)" --NoDataValue=$NODATA && mv temp.tif PH-GSNM-P-2023-30-60.tif 
-gdal_calc.py --quiet -A PH-GSNM-PH-2023-0-30.tif --outfile=temp.tif --calc="A*(A!=-9999) + ($NODATA)*(A==-9999)" --NoDataValue=$NODATA && mv temp.tif PH-GSNM-PH-2023-0-30.tif 
-gdal_calc.py --quiet -A PH-GSNM-PH-2023-30-60.tif --outfile=temp.tif --calc="A*(A!=-9999) + ($NODATA)*(A==-9999)" --NoDataValue=$NODATA && mv temp.tif PH-GSNM-PH-2023-30-60.tif 
+gdal_calc.py --quiet -A PH-GSNM-KXX-2023-0-30.tif --outfile=temp.tif --calc="A*(A!=-9999) + ($NODATA)*(A==-9999)" --NoDataValue=$NODATA && mv temp.tif PH-GSNM-KXX-2023-0-30.tif 
+gdal_calc.py --quiet -A PH-GSNM-KXX-2023-30-60.tif --outfile=temp.tif --calc="A*(A!=-9999) + ($NODATA)*(A==-9999)" --NoDataValue=$NODATA && mv temp.tif PH-GSNM-KXX-2023-30-60.tif 
+gdal_calc.py --quiet -A PH-GSNM-ORC-2023-0-30.tif --outfile=temp.tif --calc="A*(A!=-9999) + ($NODATA)*(A==-9999)" --NoDataValue=$NODATA && mv temp.tif PH-GSNM-ORC-2023-0-30.tif 
+gdal_calc.py --quiet -A PH-GSNM-ORC-2023-30-60.tif --outfile=temp.tif --calc="A*(A!=-9999) + ($NODATA)*(A==-9999)" --NoDataValue=$NODATA && mv temp.tif PH-GSNM-ORC-2023-30-60.tif 
+gdal_calc.py --quiet -A PH-GSNM-PXX-2023-0-30.tif --outfile=temp.tif --calc="A*(A!=-9999) + ($NODATA)*(A==-9999)" --NoDataValue=$NODATA && mv temp.tif PH-GSNM-PXX-2023-0-30.tif 
+gdal_calc.py --quiet -A PH-GSNM-PXX-2023-30-60.tif --outfile=temp.tif --calc="A*(A!=-9999) + ($NODATA)*(A==-9999)" --NoDataValue=$NODATA && mv temp.tif PH-GSNM-PXX-2023-30-60.tif 
+gdal_calc.py --quiet -A PH-GSNM-PHX-2023-0-30.tif --outfile=temp.tif --calc="A*(A!=-9999) + ($NODATA)*(A==-9999)" --NoDataValue=$NODATA && mv temp.tif PH-GSNM-PHX-2023-0-30.tif 
+gdal_calc.py --quiet -A PH-GSNM-PHX-2023-30-60.tif --outfile=temp.tif --calc="A*(A!=-9999) + ($NODATA)*(A==-9999)" --NoDataValue=$NODATA && mv temp.tif PH-GSNM-PHX-2023-30-60.tif 
 gdal_calc.py --quiet -A PH-GSNM-SAND-2023-0-30.tif --outfile=temp.tif --calc="A*(A!=-9999) + ($NODATA)*(A==-9999)" --NoDataValue=$NODATA && mv temp.tif PH-GSNM-SAND-2023-0-30.tif 
 gdal_calc.py --quiet -A PH-GSNM-SAND-2023-30-60.tif --outfile=temp.tif --calc="A*(A!=-9999) + ($NODATA)*(A==-9999)" --NoDataValue=$NODATA && mv temp.tif PH-GSNM-SAND-2023-30-60.tif 
 gdal_calc.py --quiet -A PH-GSNM-SILT-2023-0-30.tif --outfile=temp.tif --calc="A*(A!=-9999) + ($NODATA)*(A==-9999)" --NoDataValue=$NODATA && mv temp.tif PH-GSNM-SILT-2023-0-30.tif 
@@ -66,4 +66,4 @@ gdal_calc.py --quiet -A PH-GSOC-RSRSSM2-2021-0-30.tif --outfile=temp.tif --calc=
 gdal_calc.py --quiet -A PH-GSOC-RSRSSM2U-2021-0-30.tif --outfile=temp.tif --calc="A*(A!=-339999995214436424907732413799364296704.000) + ($NODATA)*(A==-339999995214436424907732413799364296704.000)" --NoDataValue=$NODATA && mv temp.tif PH-GSOC-RSRSSM2U-2021-0-30.tif
 gdal_calc.py --quiet -A PH-GSOC-RSRSSM3-2021-0-30.tif --outfile=temp.tif --calc="A*(A!=-339999995214436424907732413799364296704.000) + ($NODATA)*(A==-339999995214436424907732413799364296704.000)" --NoDataValue=$NODATA && mv temp.tif PH-GSOC-RSRSSM3-2021-0-30.tif
 gdal_calc.py --quiet -A PH-GSOC-RSRSSM3U-2021-0-30.tif --outfile=temp.tif --calc="A*(A!=-339999995214436424907732413799364296704.000) + ($NODATA)*(A==-339999995214436424907732413799364296704.000)" --NoDataValue=$NODATA && mv temp.tif PH-GSOC-RSRSSM3U-2021-0-30.tif
-gdal_calc.py --quiet -A PH-GSOC-T0-2021-0-30.tif --outfile=temp.tif --calc="A*(A!=-339999995214436424907732413799364296704.000) + ($NODATA)*(A==-339999995214436424907732413799364296704.000)" --NoDataValue=$NODATA && mv temp.tif PH-GSOC-T0-2021-0-30.tif
+gdal_calc.py --quiet -A PH-GSOC-T0X-2021-0-30.tif --outfile=temp.tif --calc="A*(A!=-339999995214436424907732413799364296704.000) + ($NODATA)*(A==-339999995214436424907732413799364296704.000)" --NoDataValue=$NODATA && mv temp.tif PH-GSOC-T0X-2021-0-30.tif
