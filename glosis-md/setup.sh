f4680281c707868f09747e80c244f18b8353b514
eloi=ribeiro <mail@eloiribeiro.eu>
2025-04-07 20:39:39 +0200

    fix metadata and all upload to pycsw

--- glosis-md/setup.sh
+++ glosis-md/setup.sh
@@ -5,7 +5,6 @@
 
 cd ~/Work/Code/FAO/GloSIS/glosis-md
 mkdir records
-cp /home/carva014/Downloads/FAO/SIS/PH/Processed/*.xml /home/carva014/Work/Code/FAO/GloSIS/glosis-md/records
 docker compose up
 # http://localhost:8001/
 
@@ -27,54 +26,20 @@ docker-compose exec pycsw pycsw-admin.py load-records --help
 #   --help                          Show this message and exit.
 
 #                         pycsw-admin.py load-records --config default.yml --path /path/to/records
+
+# delete records
+docker-compose exec pycsw pycsw-admin.py delete-records --config /etc/pycsw/pycsw.yml -y
+
+# load records
+python /home/carva014/Work/Code/FAO/GloSIS-private/Metadata/table2xml.py
+python /home/carva014/Work/Code/FAO/GloSIS-private/Metadata/export.py
+cp /home/carva014/Downloads/FAO/SIS/PH/Processed/*.xml /home/carva014/Work/Code/FAO/GloSIS/glosis-md/records
 docker-compose exec pycsw pycsw-admin.py load-records -c /etc/pycsw/pycsw.yml -p /records -r -y
 
-# /home/pycsw/pycsw/pycsw/core/repository.py:131: SAWarning: Did not recognize type 'geometry' of column 'wkb_geometry'
-#   self.dataset = type(
-# Traceback (most recent call last):
-#   File "/home/pycsw/pycsw/pycsw/core/admin.py", line 82, in load_records
-#     metadata_record = json.load(fh)
-#   File "/usr/local/lib/python3.10/json/__init__.py", line 293, in load
-#     return loads(fp.read(),
-#   File "/usr/local/lib/python3.10/json/__init__.py", line 346, in loads
-#     return _default_decoder.decode(s)
-#   File "/usr/local/lib/python3.10/json/decoder.py", line 337, in decode
-#     obj, end = self.raw_decode(s, idx=_w(s, 0).end())
-#   File "/usr/local/lib/python3.10/json/decoder.py", line 355, in raw_decode
-#     raise JSONDecodeError("Expecting value", s, err.value) from None
-# json.decoder.JSONDecodeError: Expecting value: line 1 column 1 (char 0)
 
-# During handling of the above exception, another exception occurred:
 
-# Traceback (most recent call last):
-#   File "/usr/local/bin/pycsw-admin.py", line 33, in <module>
-#     sys.exit(load_entry_point('pycsw', 'console_scripts', 'pycsw-admin.py')())
-#   File "/usr/local/lib/python3.10/site-packages/click/core.py", line 1161, in __call__
-#     return self.main(*args, **kwargs)
-#   File "/usr/local/lib/python3.10/site-packages/click/core.py", line 1082, in main
-#     rv = self.invoke(ctx)
-#   File "/usr/local/lib/python3.10/site-packages/click/core.py", line 1697, in invoke
-#     return _process_result(sub_ctx.command.invoke(sub_ctx))
-#   File "/usr/local/lib/python3.10/site-packages/click/core.py", line 1443, in invoke
-#     return ctx.invoke(self.callback, **ctx.params)
-#   File "/usr/local/lib/python3.10/site-packages/click/core.py", line 788, in invoke
-#     return __callback(*args, **kwargs)
-#   File "/usr/local/lib/python3.10/site-packages/click/decorators.py", line 33, in new_func
-#     return f(get_current_context(), *args, **kwargs)
-#   File "/home/pycsw/pycsw/pycsw/core/admin.py", line 430, in cli_load_records
-#     load_records(
-#   File "/home/pycsw/pycsw/pycsw/core/admin.py", line 84, in load_records
-#     metadata_record = etree.parse(recfile, context.parser)
-#   File "src/lxml/etree.pyx", line 3590, in lxml.etree.parse
-#   File "src/lxml/parser.pxi", line 1958, in lxml.etree._parseDocument
-#   File "src/lxml/parser.pxi", line 1984, in lxml.etree._parseDocumentFromURL
-#   File "src/lxml/parser.pxi", line 1887, in lxml.etree._parseDocFromFile
-#   File "src/lxml/parser.pxi", line 1200, in lxml.etree._BaseParser._parseDocFromFile
-#   File "src/lxml/parser.pxi", line 633, in lxml.etree._ParserContext._handleParseResultDoc
-#   File "src/lxml/parser.pxi", line 743, in lxml.etree._handleParseResult
-#   File "src/lxml/parser.pxi", line 672, in lxml.etree._raiseParseError
-#   File "/records/PH-GSNM-PHX-2023.xml", line 129
-# lxml.etree.XMLSyntaxError: StartTag: invalid element name, line 129, column 135
+
+
 
 # verify if records were loaded
 docker-compose exec pycsw-db psql -U glosis -d pycsw -c "\dt"
@@ -83,5 +48,10 @@ curl "http://localhost:8001/csw?service=CSW&version=2.0.2&request=GetRecords&typ
 
 
 
+# [SQL: INSERT INTO records (identifier, typename, schema, mdsource, insert_date, xml, anytext, metadata, metadata_type, language, type, title, title_alternate, abstract, edition, keywords, keywordstype, themes, parentidentifier, relation, time_begin, time_end, topicategory, resourcelanguage, creator, publisher, contributor, organization, securityconstraints, accessconstraints, otherconstraints, date, date_revision, date_creation, date_publication, date_modified, format, source, crs, geodescode, denominator, distancevalue, distanceuom, wkt_geometry, vert_extent_min, vert_extent_max, servicetype, servicetypeversion, operation, couplingtype, operateson, operatesonidentifier, operatesoname, degree, classification, conditionapplyingtoaccessanduse, lineage, responsiblepartyrole, specificationtitle, specificationdate, specificationdatetype, platform, instrument, sensortype, cloudcover, bands, links, contacts, anytext_tsvector, wkb_geometry) VALUES (%(identifier)s, %(typename)s, %(schema)s, %(mdsource)s, %(insert_date)s, %(xml)s, %(anytext)s, %(metadata)s, %(metadata_type)s, %(language)s, %(type)s, %(title)s, %(title_alternate)s, %(abstract)s, %(edition)s, %(keywords)s, %(keywordstype)s, %(themes)s, %(parentidentifier)s, %(relation)s, %(time_begin)s, %(time_end)s, %(topicategory)s, %(resourcelanguage)s, %(creator)s, %(publisher)s, %(contributor)s, %(organization)s, %(securityconstraints)s, %(accessconstraints)s, %(otherconstraints)s, %(date)s, %(date_revision)s, %(date_creation)s, %(date_publication)s, %(date_modified)s, %(format)s, %(source)s, %(crs)s, %(geodescode)s, %(denominator)s, %(distancevalue)s, %(distanceuom)s, %(wkt_geometry)s, %(vert_extent_min)s, %(vert_extent_max)s, %(servicetype)s, %(servicetypeversion)s, %(operation)s, %(couplingtype)s, %(operateson)s, %(operatesonidentifier)s, %(operatesoname)s, %(degree)s, %(classification)s, %(conditionapplyingtoaccessanduse)s, %(lineage)s, %(responsiblepartyrole)s, %(specificationtitle)s, %(specificationdate)s, %(specificationdatetype)s, %(platform)s, %(instrument)s, %(sensortype)s, %(cloudcover)s, %(bands)s, %(links)s, %(contacts)s, %(anytext_tsvector)s, %(wkb_geometry)s)]
+# [parameters: {'identifier': '32afbc7a-ebeb-11ef-bc12-6b4a6fcd8b5e', 'typename': 'gmd:MD_Metadata', 'schema': 'http://www.isotc211.org/2005/gmd', 'mdsource': 'local', 'insert_date': '2025-04-07T17:16:17Z', 'xml': '<gmd:MD_Metadata xmlns:gts="http://www.isotc211.org/2005/gts" xmlns:gml="http://www.opengis.net/gml/3.2" xmlns:gmd="http://www.isotc211.org/2005/gmd" ... (21408 characters truncated) ... n          </gmd:statement>\n        </gmd:LI_Lineage>\n      </gmd:lineage>\n    </gmd:DQ_DataQuality>\n  </gmd:dataQualityInfo>\n</gmd:MD_Metadata>', 'anytext': '  32afbc7a-ebeb-11ef-bc12-6b4a6fcd8b5e               Andrew B. Flores    Departement of Agriculture - Bureau of Soils and Water Management    Science ... (2201 characters truncated) ... 23/GLOSIS.PH-GSNM-CLAY-2023.D-30-60.tif    WWW:LINK-1.0-http--link    Download 30-60                           https://doi.org/10.4060/cc1717en      ', 'metadata': None, 'metadata_type': 'application/xml', 'language': 'eng', 'type': 'dataset', 'title': 'Soil clay content (Philippines - 250 m)', 'title_alternate': None, 'abstract': 'Percent clay derived from soil samples collected from 0-30, 30-60 and 60-100 cm depth', 'edition': None, 'keywords': 'soil,digital soil mapping,clay,texture,Soil science,Philippines', 'keywordstype': 'theme', 'themes': '[{"keywords": [{"name": "Soil science", "url": null}], "type": "discipline", "thesaurus": {"title": "Discipline", "url": null, "date": null, "datetyp ... (12 characters truncated) ... {"keywords": [{"name": "Philippines", "url": null}], "type": "place", "thesaurus": {"title": "Region", "url": null, "date": null, "datetype": null}}]', 'parentidentifier': None, 'relation': None, 'time_begin': '1960-01-01', 'time_end': '2020-12-31', 'topicategory': 'geoscientificInformation', 'resourcelanguage': 'eng', 'creator': None, 'publisher': None, 'contributor': 'Departement of Agriculture - Bureau of Soils and Water Management', 'organization': 'Departement of Agriculture - Bureau of Soils and Water Management', 'securityconstraints': None, 'accessconstraints': 'copyright', 'otherconstraints': 'Creative Commons Attribution 4.0', 'date': '2023-12-01', 'date_revision': None, 'date_creation': '2023-12-01', 'date_publication': '2023-12-01', 'date_modified': '2023-12-01', 'format': 'GeoTIFF,OGC:WMTS,WWW:LINK-1.0-http--link', 'source': None, 'crs': 'urn:ogc:def:crs:EPSG::32651', 'geodescode': None, 'denominator': None, 'distancevalue': '250.0', 'distanceuom': 'm', 'wkt_geometry': 'POLYGON((116.50 4.60, 116.50 21.10, 126.60 21.10, 126.60 4.60, 116.50 4.60))', 'vert_extent_min': None, 'vert_extent_max': None, 'servicetype': '', 'servicetypeversion': None, 'operation': None, 'couplingtype': None, 'operateson': None, 'operatesonidentifier': None, 'operatesoname': None, 'degree': None, 'classification': None, 'conditionapplyingtoaccessanduse': None, 'lineage': None, 'responsiblepartyrole': 'metadataProvider', 'specificationtitle': None, 'specificationdate': None, 'specificationdatetype': None, 'platform': None, 'instrument': None, 'sensortype': None, 'cloudcover': None, 'bands': None, 'links': '[{"name": "Web Map Tile Service (self hosted)", "description": null, "protocol": "OGC:WMTS", "url": "http://localhost:8082/?map=/etc/mapserver/PH-GSN ... (1392 characters truncated) ... 6&WIDTH=762&HEIGHT=1228&LAYERS=PH-GSNM-CLAY-2023-0-30&STYLES=&FORMAT=image%2Fpng&DPI=96&MAP_RESOLUTION=96&FORMAT_OPTIONS=dpi%3A96&TRANSPARENT=TRUE"}]', 'contacts': '[{"name": "Andrew B. Flores", "name_url": "", "organization": "Departement of Agriculture - Bureau of Soils and Water Management", "organization_url" ... (163 characters truncated) ... n", "region": null, "postcode": "1128", "country": "Philippines", "email": "andrew.flores@bswm.da.gov.ph", "onlineresource": null, "role": "author"}]', 'anytext_tsvector': None, 'wkb_geometry': None}]
+# (Background on this error at: https://sqlalche.me/e/14/gkpj)
+# (psycopg2.errors.UniqueViolation) duplicate key value violates unique constraint "records_pkey"
+# DETAIL:  Key (identifier)=(32afbc81-ebeb-11ef-bc12-6b4a6fcd8b5e) already exists.
 
 
