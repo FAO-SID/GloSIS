f8cceca42b4ab2866459d2b9c4c51b4086c4a33b
eloi=ribeiro <mail@eloiribeiro.eu>
2025-04-11 12:54:01 +0200

    db backup layer info fro wm

--- deploy.sh
+++ deploy.sh
@@ -42,7 +42,7 @@ docker exec -it glosis-db psql -d glosis -U glosis -f /tmp/init-03.sql
 ####################
 
 # load records
-cp /home/carva014/Work/Code/FAO/GloSIS/glosis-datacube/PH/output/*.xml /home/carva014/Work/Code/FAO/GloSIS/glosis-md/records
+mv /home/carva014/Work/Code/FAO/GloSIS/glosis-datacube/PH/output/*.xml /home/carva014/Work/Code/FAO/GloSIS/glosis-md/records
 docker-compose exec glosis-md ls -l /records
 docker-compose exec glosis-md pycsw-admin.py load-records -c /etc/pycsw/pycsw.yml -p /records -r -y
 
@@ -60,8 +60,8 @@ docker-compose exec glosis-md sed -i 's/https:\/\/pycsw.org/http:\/\/localhost:8
 ####################
 
 # copy geotiffs and map files
-cp /home/carva014/Work/Code/FAO/GloSIS/glosis-datacube/PH/output/*.tif /home/carva014/Work/Code/FAO/GloSIS/glosis-ws/data/
-cp /home/carva014/Work/Code/FAO/GloSIS/glosis-datacube/PH/output/*.map /home/carva014/Work/Code/FAO/GloSIS/glosis-ws/data/
+mv /home/carva014/Work/Code/FAO/GloSIS/glosis-datacube/PH/output/*.tif /home/carva014/Work/Code/FAO/GloSIS/glosis-ws/data/
+mv /home/carva014/Work/Code/FAO/GloSIS/glosis-datacube/PH/output/*.map /home/carva014/Work/Code/FAO/GloSIS/glosis-ws/data/
 
 # Test WMS
 # GetCapabilities
@@ -74,3 +74,11 @@ http://localhost:8082/?map=/etc/mapserver/PH-GSAS-ECX-2020-0-30.map&SERVICE=WMS&
 http://localhost:8082/?map=/etc/mapserver/PH-GSAS-ECX-2020-0-30.map&SERVICE=WMS&VERSION=1.3.0&REQUEST=GetFeatureInfo&BBOX=1.16625995882351496%2C116.25895549999999901%2C24.6476970411764853%2C127.10635850000001312&CRS=EPSG%3A4326&WIDTH=595&HEIGHT=1288&LAYERS=PH-GSAS-ECX-2020-0-30&STYLES=&FORMAT=image%2Fpng&QUERY_LAYERS=PH-GSAS-ECX-2020-0-30&INFO_FORMAT=text%2Fhtml&I=282&J=429
 # QGIS add WMS layer
 http://localhost:8082/?map=/etc/mapserver/PH-GSAS-ECX-2020-0-30.map
+
+
+####################
+#  PostgreSQL (db) #
+####################
+
+pg_dump -h localhost -p 5442 -U glosis -d glosis -Fc -v -f /home/carva014/Work/Code/FAO/GloSIS/glosis-db/backups/glosis_backup_2025-04-11.backup
+
