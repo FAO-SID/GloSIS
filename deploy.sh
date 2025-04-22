e8ea0edd065dd1ebedde95b9993cf4f45f235543
eloi=ribeiro <mail@eloiribeiro.eu>
2025-04-22 15:37:26 +0200

    ip

--- deploy.sh
+++ deploy.sh
@@ -188,7 +188,7 @@ docker exec -it glosis-db psql -d glosis -U glosis -c "
 ####################
 
 # load records
-mv $PROJECT_DIR/GloSIS/glosis-datacube/PH/output/*.xml $PROJECT_DIR/GloSIS/glosis-md/records
+# mv $PROJECT_DIR/GloSIS/glosis-datacube/PH/output/*.xml $PROJECT_DIR/GloSIS/glosis-md/records
 docker-compose exec glosis-md ls -l /records
 docker-compose exec glosis-md pycsw-admin.py load-records -c /etc/pycsw/pycsw.yml -p /records -r -y
 
@@ -196,9 +196,9 @@ docker-compose exec glosis-md pycsw-admin.py load-records -c /etc/pycsw/pycsw.ym
 docker-compose exec glosis-db psql -U glosis -d glosis -c "SELECT identifier, title FROM pycsw.records ORDER BY title;"
 
 # custumization - https://docs.pycsw.org/en/latest/configuration.html
-docker-compose exec glosis-md sed -i 's/pycsw website/Philippines SIS metadata/g' pycsw/pycsw/ogc/api/templates/_base.html
-docker-compose exec glosis-md sed -i 's/https:\/\/pycsw.org\/img\/pycsw-logo-vertical.png/https:\/\/www.bswm.da.gov.ph\/wp-content\/uploads\/BAGONG-PILIPINAS.png/g' pycsw/pycsw/ogc/api/templates/_base.html
-docker-compose exec glosis-md sed -i 's/https:\/\/pycsw.org/http:\/\/localhost:8001\/collections\/metadata:main\/items/g' pycsw/pycsw/ogc/api/templates/_base.html
+# docker-compose exec glosis-md sed -i 's/pycsw website/Philippines SIS metadata/g' pycsw/pycsw/ogc/api/templates/_base.html
+# docker-compose exec glosis-md sed -i 's/https:\/\/pycsw.org\/img\/pycsw-logo-vertical.png/https:\/\/www.bswm.da.gov.ph\/wp-content\/uploads\/BAGONG-PILIPINAS.png/g' pycsw/pycsw/ogc/api/templates/_base.html
+# docker-compose exec glosis-md sed -i 's/https:\/\/pycsw.org/http:\/\/localhost:8001\/collections\/metadata:main\/items/g' pycsw/pycsw/ogc/api/templates/_base.html
 
 
 ####################
@@ -206,8 +206,8 @@ docker-compose exec glosis-md sed -i 's/https:\/\/pycsw.org/http:\/\/localhost:8
 ####################
 
 # copy geotiffs and map files
-mv $PROJECT_DIR/GloSIS/glosis-datacube/PH/output/*.tif $PROJECT_DIR/GloSIS/glosis-ws/data/
-mv $PROJECT_DIR/GloSIS/glosis-datacube/PH/output/*.map $PROJECT_DIR/GloSIS/glosis-ws/data/
+# mv $PROJECT_DIR/GloSIS/glosis-datacube/PH/output/*.tif $PROJECT_DIR/GloSIS/glosis-ws/data/
+# mv $PROJECT_DIR/GloSIS/glosis-datacube/PH/output/*.map $PROJECT_DIR/GloSIS/glosis-ws/data/
 
 # Test raster WMS
 # GetCapabilities
