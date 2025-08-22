cbd52f036532844cf976245a439d0c064d1276e9
eloi=ribeiro <mail@eloiribeiro.eu>
2025-08-22 13:51:18 +0200

    upper an lower depth up to 10 mv

--- glosis-db/modifications/mod_006.sql
+++ glosis-db/modifications/mod_006.sql
@@ -4,11 +4,11 @@
 
 ALTER TABLE IF EXISTS core.specimen ADD CHECK (lower_depth > upper_depth);
 ALTER TABLE IF EXISTS core.specimen ADD CHECK (upper_depth >= 0);
-ALTER TABLE IF EXISTS core.specimen ADD CHECK (upper_depth <= 500);
+ALTER TABLE IF EXISTS core.specimen ADD CHECK (upper_depth <= 1000);
 
 ALTER TABLE IF EXISTS core.element DROP CONSTRAINT element_check;
 ALTER TABLE IF EXISTS core.element DROP CONSTRAINT element_upper_depth_check;
 
 ALTER TABLE IF EXISTS core.element ADD CHECK (lower_depth > upper_depth);
 ALTER TABLE IF EXISTS core.element ADD CHECK (upper_depth >= 0);
-ALTER TABLE IF EXISTS core.element ADD CHECK (upper_depth <= 500);
+ALTER TABLE IF EXISTS core.element ADD CHECK (upper_depth <= 1000);
