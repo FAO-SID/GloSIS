e68994cfc056c3569896b653cecb3ecaf7803f4f
eloi=ribeiro <mail@eloiribeiro.eu>
2025-08-28 09:39:12 +0200

    rm GSAS high uncert

--- glosis-datacube/BT/scripts/data_cube_1_rename.sh
+++ glosis-datacube/BT/scripts/data_cube_1_rename.sh
@@ -110,3 +110,72 @@ INPUT_DIR="/home/carva014/Work/Code/FAO/GloSIS/glosis-datacube/BT/input/OTHER"  
 OUTPUT_DIR="/home/carva014/Work/Code/FAO/GloSIS/glosis-datacube/BT/tmp"                       # << EDIT THIS LINE!
 YEAR=2024                                                                                     # << EDIT THIS LINE!
 cp $INPUT_DIR/SoilType_map_10.tif $OUTPUT_DIR/BT-OTHER-CLAWRB-$YEAR-0-100.tif
+
+
+
+GSNM	AvailK_0_30_mean.tiff	Potassium (K+) - exchangeable	mean	2024	0	30
+GSNM	AvailK_0_30_sd.tiff	Potassium (K+) - exchangeable	standard deviation	2024	0	30
+GSNM	AvailP_0_30_mean.tiff	Phosphorus (P) - exchangeable	mean	2024	0	30
+GSNM	AvailP_0_30_sd.tiff	Phosphorus (P) - exchangeable	standard deviation	2024	0	30
+GSNM	BD_0_30_mean.tiff	Bulk density	mean	2024	0	30
+GSNM	BD_0_30_sd.tiff	Bulk density	standard deviation	2024	0	30
+GSNM	BS_0_30_mean.tiff	Base saturation	mean	2024	0	30
+GSNM	BS_0_30_sd.tiff	Base saturation	standard deviation	2024	0	30
+GSNM	Ca_0_30_mean.tiff	Calcium (Ca++) - exchangeable	mean	2024	0	30
+GSNM	Ca_0_30_sd.tiff	Calcium (Ca++) - exchangeable	standard deviation	2024	0	30
+GSNM	CEC_0_30_mean.tiff	Cation exchange capacity	mean	2024	0	30
+GSNM	CEC_0_30_sd.tiff	Cation exchange capacity	standard deviation	2024	0	30
+GSNM	ClayTotal_0_30_mean.tiff	Clay texture fraction	mean	2024	0	30
+GSNM	ClayTotal_0_30_sd.tiff	Clay texture fraction	standard deviation	2024	0	30
+GSNM	CNRatio_0_30_mean.tiff	Carbon/Nitrogen (C/N) ratio	mean	2024	0	30
+GSNM	CNRatio_0_30_sd.tiff	Carbon/Nitrogen (C/N) ratio	standard deviation	2024	0	30
+GSNM	CoarMat_0_30_mean.tiff	Coarse fragments - field class	mean	2024	0	30
+GSNM	CoarMat_0_30_sd.tiff	Coarse fragments - field class	standard deviation	2024	0	30
+GSNM	K_0_30_mean.tiff	Potassium (K+) - exchangeable	mean	2024	0	30
+GSNM	K_0_30_sd.tiff	Potassium (K+) - exchangeable	standard deviation	2024	0	30
+GSNM	Mg2_0_30_mean.tiff	Magnesium (Mg++) - exchangeable	mean	2024	0	30
+GSNM	Mg2_0_30_sd.tiff	Magnesium (Mg++) - exchangeable	standard deviation	2024	0	30
+GSNM	Na_0_30_mean.tiff	Sodium (Na+) - exchangeable	mean	2024	0	30
+GSNM	Na_0_30_sd.tiff	Sodium (Na+) - exchangeable	standard deviation	2024	0	30
+GSNM	OC_0_30_mean.tiff	Carbon (C) - organic	mean	2024	0	30
+GSNM	OC_0_30_sd.tiff	Carbon (C) - organic	standard deviation	2024	0	30
+GSNM	pHH2O_0_30_mean.tiff	pH - Hydrogen potential in water	mean	2024	0	30
+GSNM	pHH2O_0_30_sd.tiff	pH - Hydrogen potential in water	standard deviation	2024	0	30
+GSNM	SandTotal_0_30_mean.tiff	Sand texture fraction	mean	2024	0	30
+GSNM	SandTotal_0_30_sd.tiff	Sand texture fraction	standard deviation	2024	0	30
+GSNM	SiltTotal_0_30_mean.tiff	Silt texture fraction	mean	2024	0	30
+GSNM	SiltTotal_0_30_sd.tiff	Silt texture fraction	standard deviation	2024	0	30
+GSNM	TEB_0_30_mean.tiff	Total Exchangeable Bases	mean	2024	0	30
+GSNM	TEB_0_30_sd.tiff	Total Exchangeable Bases	standard deviation	2024	0	30
+GSNM	TotalN_0_30_mean.tiff	Nitrogen (N) - total	mean	2024	0	30
+GSNM	TotalN_0_30_sd.tiff	Nitrogen (N) - total	standard deviation	2024	0	30
+						
+GSOC	Bhutan_GSOCseq_AbsDiff_BAU_Map030.tif	GSOCseq - absolute difference business as usual	mean	2022	0	30
+GSOC	Bhutan_GSOCseq_AbsDiff_SSM1_Map030.tif	GSOCseq - absolute difference SSM1	mean	2022	0	30
+GSOC	Bhutan_GSOCseq_AbsDiff_SSM2_Map030.tif	GSOCseq - absolute difference SSM2	mean	2022	0	30
+GSOC	Bhutan_GSOCseq_AbsDiff_SSM3_Map030.tif	GSOCseq - absolute difference SSM3	mean	2022	0	30
+GSOC	Bhutan_GSOCseq_ASR_BAU_Map030.tif	GSOCseq - ASR business as usual	mean	2022	0	30
+GSOC	Bhutan_GSOCseq_ASR_BAU_UncertaintyMap030.tif	GSOCseq - ASR business as usual	uncertainty	2022	0	30
+GSOC	Bhutan_GSOCseq_ASR_SSM1_Map030.tif	GSOCseq - ASR SSM1	mean	2022	0	30
+GSOC	Bhutan_GSOCseq_ASR_SSM1_UncertaintyMap030.tif	GSOCseq - ASR SSM1	uncertainty	2022	0	30
+GSOC	Bhutan_GSOCseq_ASR_SSM2_Map030.tif	GSOCseq - ASR SSM2	mean	2022	0	30
+GSOC	Bhutan_GSOCseq_ASR_SSM2_UncertaintyMap030.tif	GSOCseq - ASR SSM2	uncertainty	2022	0	30
+GSOC	Bhutan_GSOCseq_ASR_SSM3_Map030.tif	GSOCseq - ASR SSM3	mean	2022	0	30
+GSOC	Bhutan_GSOCseq_ASR_SSM3_UncertaintyMap030.tif	GSOCseq - ASR SSM3	uncertainty	2022	0	30
+GSOC	Bhutan_GSOCseq_BAU_UncertaintyMap030.tif	GSOCseq - SOC business as usual	uncertainty	2022	0	30
+GSOC	Bhutan_GSOCseq_finalSOC_BAU_Map030.tif	GSOCseq - SOC business as usual	mean	2022	0	30
+GSOC	Bhutan_GSOCseq_finalSOC_SSM1_Map030.tif	GSOCseq - SOC SSM1	mean	2022	0	30
+GSOC	Bhutan_GSOCseq_finalSOC_SSM2_Map030.tif	GSOCseq - SOC SSM2	mean	2022	0	30
+GSOC	Bhutan_GSOCseq_finalSOC_SSM3_Map030.tif	GSOCseq - SOC SSM3	mean	2022	0	30
+GSOC	Bhutan_GSOCseq_RelDiff_SSM1_Map030.tif	GSOCseq - relative difference SSM1	mean	2022	0	30
+GSOC	Bhutan_GSOCseq_RelDiff_SSM2_Map030.tif	GSOCseq - relative difference SSM2	mean	2022	0	30
+GSOC	Bhutan_GSOCseq_RelDiff_SSM3_Map030.tif	GSOCseq - relative difference SSM3	mean	2022	0	30
+GSOC	Bhutan_GSOCseq_RSR_SSM1_Map030.tif	GSOCseq - RSR SSM1	mean	2022	0	30
+GSOC	Bhutan_GSOCseq_RSR_SSM1_UncertaintyMap030.tif	GSOCseq - RSR SSM1	uncertainty	2022	0	30
+GSOC	Bhutan_GSOCseq_RSR_SSM2_Map030.tif	GSOCseq - RSR SSM2	mean	2022	0	30
+GSOC	Bhutan_GSOCseq_RSR_SSM2_UncertaintyMap030.tif	GSOCseq - RSR SSM2	uncertainty	2022	0	30
+GSOC	Bhutan_GSOCseq_RSR_SSM3_Map030.tif	GSOCseq - RSR SSM3	mean	2022	0	30
+GSOC	Bhutan_GSOCseq_RSR_SSM3_UncertaintyMap030.tif	GSOCseq - RSR SSM3	uncertainty	2022	0	30
+GSOC	Bhutan_GSOCseq_SSM_UncertaintyMap030.tif	GSOCseq - SSM	uncertainty	2022	0	30
+GSOC	Bhutan_GSOCseq_T0_Map030.tif	GSOCseq - time zero	mean	2022	0	30
+GSOC	Bhutan_GSOCseq_T0_UncertaintyMap030.tif	GSOCseq - time zero	uncertainty	2022	0	30
