f3fdf7593f1683eabe725470363c7dce22880601
eloi=ribeiro <mail@eloiribeiro.eu>
2025-08-29 19:19:35 +0200

    BT md

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Variables
-PROJECT_DIR="/home/carva014/Work/Code/FAO/GloSIS"      # << EDIT THIS LINE!
+PROJECT_DIR="/home/carva014/Work/Code/FAO/GloSIS"
 COUNTRY=BT
 COUNTRY_LONG="Bhutan"
 ORG_LOGO_URL="https:\/\/tse4.mm.bing.net\/th\/id/OIP.hV37F63PxOkqMwTAlCNnvQAAAA?r=0&pid=Api" # PH "https:\/\/www.bswm.da.gov.ph\/wp-content\/uploads\/BAGONG-PILIPINAS.png"
@@ -70,12 +70,12 @@ docker cp $PROJECT_DIR/glosis-shiny/global/global.R glosis-shiny:/srv/shiny-serv
 ####################
 
 # Load records
-# rm $PROJECT_DIR/glosis-ws/volume/*.map
-# rm $PROJECT_DIR/glosis-ws/volume/*.tif
+rm $PROJECT_DIR/glosis-ws/volume/*.map
+rm $PROJECT_DIR/glosis-ws/volume/*.tif
 cp $PROJECT_DIR/glosis-datacube/$COUNTRY/output/*.tif $PROJECT_DIR/glosis-ws/volume
 cp $PROJECT_DIR/glosis-datacube/$COUNTRY/output/*.map $PROJECT_DIR/glosis-ws/volume
 
-# Build and startcontainer
+# Build and start container
 docker compose up --build glosis-ws -d
 
 
@@ -83,22 +83,28 @@ docker compose up --build glosis-ws -d
 #     glosis-md    #
 ####################
 
-# Build and startcontainer
+# Customize pyCSW
+
+# Build and start container
 docker compose up --build glosis-md -d
 
+# Customize pyCSW UI - https://docs.pycsw.org/en/latest/configuration.html
+cp $PROJECT_DIR/glosis-md/pycsw_default.yml $PROJECT_DIR/glosis-md/pycsw.yml
+sed -i "s|COUNTRY_SIS|$COUNTRY_LONG|g" $PROJECT_DIR/glosis-md/pycsw.yml
+docker compose exec glosis-md sed -i "s/pycsw website/${COUNTRY_LONG} SIS metadata/g" pycsw/pycsw/ogc/api/templates/_base.html
+docker compose exec glosis-md sed -i "s|https://pycsw.org/img/pycsw-logo-vertical.png|${ORG_LOGO_URL}|g" pycsw/pycsw/ogc/api/templates/_base.html
+docker compose exec glosis-md sed -i "s/https:\/\/pycsw.org/http:\/\/localhost:8001\/collections\/metadata:main\/items/g" pycsw/pycsw/ogc/api/templates/_base.html
+
 # Load records
+docker compose exec glosis-db psql -U glosis -d glosis -c "DELETE FROM pycsw.records;"
 rm $PROJECT_DIR/glosis-md/volume/*.xml
 cp $PROJECT_DIR/glosis-datacube/$COUNTRY/output/*.xml $PROJECT_DIR/glosis-md/volume
 docker compose exec glosis-md ls -l /records
-# FAILED: docker compose exec glosis-md pycsw-admin.py load-records -c /etc/pycsw/pycsw.yml -p /records -r -y
+docker compose exec glosis-md pycsw-admin.py load-records -c /etc/pycsw/pycsw.yml -p /records -r -y
 
 # Verify if records were loaded
 docker compose exec glosis-db psql -U glosis -d glosis -c "SELECT identifier, title FROM pycsw.records ORDER BY title LIMIT 5;"
 
-# Customize pyCSW UI - https://docs.pycsw.org/en/latest/configuration.html
-docker compose exec glosis-md sed -i "s/pycsw website/${COUNTRY_LONG} SIS metadata/g" pycsw/pycsw/ogc/api/templates/_base.html
-docker compose exec glosis-md sed -i "s|https://pycsw.org/img/pycsw-logo-vertical.png|${ORG_LOGO_URL}|g" pycsw/pycsw/ogc/api/templates/_base.html
-docker compose exec glosis-md sed -i "s/https:\/\/pycsw.org/http:\/\/localhost:8001\/collections\/metadata:main\/items/g" pycsw/pycsw/ogc/api/templates/_base.html
 
 ####################
 #     glosis-wm    #
@@ -110,11 +116,12 @@ docker compose exec glosis-md sed -i "s/https:\/\/pycsw.org/http:\/\/localhost:8
 # default layer map main.js line 475
 
 # set the center and zoom level of the map
+cp $PROJECT_DIR/glosis-wm/src/js/main.default $PROJECT_DIR/glosis-wm/src/js/main.js
 sed -i "s/MAP_CENTER_LONG/$LONGITUDE/g" $PROJECT_DIR/glosis-wm/src/js/main.js # line 98
 sed -i "s/MAP_CENTER_LAT/$LATITUDE/g" $PROJECT_DIR/glosis-wm/src/js/main.js   # line 98
 sed -i "s/MAP_ZOOM/$ZOOM/g" $PROJECT_DIR/glosis-wm/src/js/main.js             # line 99
 
-# Build and startcontainer
+# Build and start container
 docker compose up --build glosis-wm -d
 
 
